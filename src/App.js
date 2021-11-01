06ea55b87b7e004005ff93ad594fe6fdfe6afb3d
tanmay-choudhary <tanmaychoudhary4102@gmail.com>
2021-11-01 23:55:08 +0530

    *app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,202 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+/*import React, { useState ,useEffect} from 'react';
+
+
+export default function App() 
+{ 
+
+
+
+  const [string , setString] = useState("https://source.unsplash.com/collection/559527") ;
+
+  const arr = ["https://source.unsplash.com/collection/559527","https://source.unsplash.com/collection/402504"];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+       setString(
+           function (prevLink){
+             if(prevLink===arr[0])
+             {
+               return arr[1];
+             }
+             else
+             {
+               return arr[0];
+             }
+           }
+       )
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     
+    <div>
+
+      <img src = {string} /> 
+     
     </div>
-  );
+
+
+  )
+
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React, { useState ,useEffect} from 'react';
+
+
+export default function App() 
+{ 
+
+
+
+  const [string , setString] = useState("") ;
+
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+       setString(
+         function (prevString){
+
+          return prevString + " *";
+
+         }
+       )
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
+
+
+
+
+  return (
+
+     
+    <div>
+
+      <h1>
+
+        {string}
+      </h1>
+     
+    </div>
+
+
+  )
+
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+/*import React, { useState } from 'react';
+
+
+export default function App() 
+{ 
+  const [count , setCount] = useState(" ") ;
+
+  function Increment(){
+    setCount(
+
+      function (prevSymbol){
+        return prevSymbol+ " *";
+      }
+    )
+  }
+
+  return (
+
+     
+    <div>
+
+      <h1>
+
+        {count }
+      </h1>
+      <button onClick = {Increment}> 
+        incrememnt 
+      </button>
+    </div>
+
+
+  )
+
+}*/
+
+
+/*import React, { useState } from 'react';
+
+
+export default function App() 
+{ 
+  const [count , setCount] = useState(0) ;
+
+  function Increment(){
+    setCount(
+
+      function (prevCount){
+        return prevCount+1;
+      }
+    )
+  }
+
+  return (
+
+     
+    <div>
+
+      <h1>
+
+        {count }
+      </h1>
+      <button onClick = {Increment}> 
+        incrememnt 
+      </button>
+    </div>
+
+
+  )
+
+}*/
